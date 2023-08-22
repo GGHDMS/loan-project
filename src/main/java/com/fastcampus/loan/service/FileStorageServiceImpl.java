f1f3f83a83b9eb773d69fc8b7d777d34cb5c04c4
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-08-22 14:35:52 +0900

    대출 신청 서류 매핑 기능 구현

--- src/main/java/com/fastcampus/loan/service/FileStorageServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/FileStorageServiceImpl.java
@@ -3,6 +3,7 @@ package com.fastcampus.loan.service;
 
 import com.fastcampus.loan.exception.BaseException;
 import com.fastcampus.loan.exception.ResultType;
+import com.fastcampus.loan.repository.ApplicationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -24,10 +25,21 @@ public class FileStorageServiceImpl implements FileStorageService{
     @Value("${spring.servlet.multipart.location}")
     private String uploadPath;
 
+    private final ApplicationRepository applicationRepository;
+
     @Override
-    public void save(MultipartFile file) {
+    public void save(Long applicationId, MultipartFile file) {
+        if (isApplicationNotPresent(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
         try {
-            Files.copy(file.getInputStream(), Paths.get(uploadPath).resolve(file.getOriginalFilename()),
+            String applicationPath = uploadPath.concat("/" + applicationId);
+            Path directoryPath = Path.of(applicationPath);
+            if (!Files.exists(directoryPath)) {
+                Files.createDirectories(directoryPath);
+            }
+
+            Files.copy(file.getInputStream(), directoryPath.resolve(file.getOriginalFilename()),
                     StandardCopyOption.REPLACE_EXISTING);
         } catch (Exception e) {
             throw new BaseException(ResultType.SYSTEM_ERROR);
@@ -35,9 +47,13 @@ public class FileStorageServiceImpl implements FileStorageService{
     }
 
     @Override
-    public Resource load(String fileName) {
+    public Resource load(Long applicationId, String fileName) {
+        if (isApplicationNotPresent(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
         try {
-            Path file = Paths.get(uploadPath).resolve(fileName);
+            String applicationPath = uploadPath.concat("/" + applicationId);
+            Path file = Paths.get(applicationPath).resolve(fileName);
             Resource resource = new UrlResource(file.toUri());
 
             if (resource.isReadable() || resource.exists()) {
@@ -51,16 +67,29 @@ public class FileStorageServiceImpl implements FileStorageService{
     }
 
     @Override
-    public Stream<Path> loadAll() {
+    public Stream<Path> loadAll(Long applicationId) {
+        if (isApplicationNotPresent(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+        String applicationPath = uploadPath.concat("/" + applicationId);
         try {
-            return Files.walk(Paths.get(uploadPath), 1).filter(path -> !path.equals(Paths.get(uploadPath)));
+            return Files.walk(Paths.get(applicationPath), 1).filter(path -> !path.equals(Paths.get(applicationPath)));
         } catch (Exception e) {
             throw new BaseException(ResultType.SYSTEM_ERROR);
         }
     }
 
     @Override
-    public void deleteAll() {
-        FileSystemUtils.deleteRecursively(Paths.get(uploadPath).toFile());
+    public void deleteAll(Long applicationId) {
+        if (isApplicationNotPresent(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+        String applicationPath = uploadPath.concat("/" + applicationId);
+        FileSystemUtils.deleteRecursively(Paths.get(applicationPath).toFile());
+    }
+
+    private boolean isApplicationNotPresent(Long applicationId) {
+        return applicationRepository.findById(applicationId).isEmpty();
     }
 }
+
